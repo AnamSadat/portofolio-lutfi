106f42171399aea723f0a7dc0780ce9db59ac6be
AnamSadat <anamsadat3@gmail.com>
2025-12-28 13:57:54 +0700

    setup primevue and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from "@primeuix/themes/aura";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["./app/assets/css/main.css"],
   modules: ["@primevue/nuxt-module"],
   primevue: {
     options: {
